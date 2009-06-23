bd5b5930ecab22f51d697a9827fd84e6d33a2d01
financeCoding@gmail.com <financeCoding@gmail.com@0ce9f64a-5edf-11de-8abc-d1c337b90d21>
2009-06-23 06:35:42 +0000

    Finished up the getters for the TransactionEntry object.

--- trunk/GoogleFinanceDemo/Finance/TransactionEntry.cs
+++ trunk/GoogleFinanceDemo/Finance/TransactionEntry.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Google.GData.Client;
+using Google.GData.Extensions;
 
 namespace Finance
 {
@@ -11,7 +12,9 @@ namespace Finance
     public enum TransactionTypes
     {
         BUY,
-        SELL
+        SELL,
+        BUYTOCONVERT,
+        SELLSHORT
     }
 
     public class TransactionEntry : AbstractEntry
@@ -31,20 +34,78 @@ namespace Finance
 
         }
 
-        // TODO: Create real types, the strings are temporary. 
+        // TODO: Create real types, the strings are temporary. Also create exceptions for handling errors of getters.
 
-        public TransactionTypes Type { get; set; }
-        public DateTime Date { get; set; }
-        public double Shares { get; set; }
-        public string Notes { get; set; }
+        public TransactionTypes Type {
+            
+            get
+            {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                string ttype = ((XmlExtension)i).Node.Attributes["type"].Value;
+                // XXX: need to handle this in a more elegant way. This is nasty and the enum still has problems
+                // converting back to selected string. Use Attributes on the enum to convert enum values back
+                // to proper strings.
+                if (ttype == "Sell Short")
+                {
+                    return TransactionTypes.SELLSHORT;
+                }
+                else if (ttype == "Buy to Cover")
+                {
+                    return TransactionTypes.BUYTOCONVERT;
+                }
+                else if (ttype == "Buy")
+                {
+                    return TransactionTypes.BUY;
+                }
+                else if (ttype == "Sell")
+                {
+                    return TransactionTypes.SELL;
+                }
+
+                throw new Exception("TransactionType Not Found");
+            }
+        }
+
+        public DateTime Date { 
+            get {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                DateTime dt = DateTime.Parse(((XmlExtension)i).Node.Attributes["date"].Value);
+                return dt;
+            } 
+        }
+        public double Shares
+        {
+            get
+            {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                return double.Parse(((XmlExtension)i).Node.Attributes["shares"].Value);
+            }
+        }
+        public string Notes
+        {
+            get
+            {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                return ((XmlExtension)i).Node.Attributes["notes"].Value;
+            }
+        }
         public Commission Commissions
         {
-            get { return null; }
+            get {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                Commission co = new Commission();
+                co = co.CreateInstance(((XmlExtension)i).Node["gf:commission"], new AtomFeedParser()) as Commission;
+                return co; }
         }
 
         public Price Price
         {
-            get { return null; }
+            get {
+                IExtensionElementFactory i = FindExtension("transactionData", "http://schemas.google.com/finance/2007");
+                Price pr = new Price();
+                pr = pr.CreateInstance(((XmlExtension)i).Node["gf:price"], new AtomFeedParser()) as Price;    
+                return pr; 
+            }
         }
 
 
