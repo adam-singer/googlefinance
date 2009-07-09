ba93d896562678d648f3bc9c566c6fd77eae58e7
financeCoding@gmail.com <financeCoding@gmail.com@0ce9f64a-5edf-11de-8abc-d1c337b90d21>
2009-07-09 04:13:43 +0000

    Added the stock ticker solution. It is not finished yet still needs a lot of structure and work.

---
+++ trunk/StockTicker/StockTicker/Commands/ActionCommands.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace StockTicker.Commands
+{
+    public class ActionCommands
+    {
+        public static RoutedUICommand LoginCommand { private set; get; }
+        public static RoutedUICommand LogoutCommand { private set; get; }
+        public static RoutedUICommand ExitCommand { private set; get; }
+
+        public static RoutedUICommand CreatePortfolio { private set; get; }
+        public static RoutedUICommand RenamePortfolio { private set; get; }
+        public static RoutedUICommand DeletePortfolio { private set; get; }
+
+        /// <summary>
+        /// Static constructor.
+        /// Creates several Routed UI commands with and without shortcut keys.
+        /// </summary>
+        static ActionCommands()
+        {
+            LoginCommand = MakeRoutedUICommand("Login", Key.I, "Ctrl+I");
+            LogoutCommand = MakeRoutedUICommand("Logout", Key.O, "Crtl+O");
+            ExitCommand = MakeRoutedUICommand("Exit");
+
+            CreatePortfolio = MakeRoutedUICommand("CreatePortfolio", Key.N, "Crtl+N");
+            RenamePortfolio = MakeRoutedUICommand("RenamePortfolio", Key.R, "Crtl+R");
+            DeletePortfolio = MakeRoutedUICommand("DeletePortfolio", Key.D, "Crtl+D");
+
+        }
+
+        /// <summary>
+        /// Creates a routed command instance without shortcut key.
+        /// </summary>
+        /// <param name="name">Given name.</param>
+        /// <returns>The routed UI command.</returns>
+        private static RoutedUICommand MakeRoutedUICommand(string name)
+        {
+            return new RoutedUICommand(name, name, typeof(ActionCommands));
+        }
+
+        /// <summary>
+        /// Creates a routed command instance with a shortcut key.
+        /// </summary>
+        /// <param name="name">Given name.</param>
+        /// <param name="key">Shortcut key.</param>
+        /// <param name="displayString">The Routed UI command.</param>
+        /// <returns></returns>
+        private static RoutedUICommand MakeRoutedUICommand(string name, Key key, string displayString)
+        {
+            InputGestureCollection gestures = new InputGestureCollection();
+            gestures.Add(new KeyGesture(key, ModifierKeys.Control, displayString));
+
+            return new RoutedUICommand(name, name, typeof(ActionCommands), gestures);
+        }
+    }
+}
--- trunk/StockTicker/StockTicker/WindowLogin.xaml.cs
+++ trunk/StockTicker/StockTicker/WindowLogin.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace StockTicker
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for WindowLogin.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class WindowLogin : Window
     {
-        public Window1()
+        public WindowLogin()
         {
             InitializeComponent();
         }
