04130c4fe02ca0686d324d7c99984352d41a1a1b
financeCoding <financeCoding@gmail.com>
2009-07-09 03:39:35 +0000

    Started a stock ticker wpf application that uses the google finance library in C#

---
+++ StockTicker/StockModel/BusinessModelObjects/BaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.ComponentModel;
+
+
+namespace StockModel.BusinessModelObjects
+{
+    /// <summary>
+    /// Abstract base class for business object models.
+    /// </summary>
+    public abstract class BaseModel : INotifyPropertyChanged
+    {
+        // Dispatcher associates with model;
+        protected Dispatcher _dispatcher;
+    
+    
+        
+    
+    }
+}
---
+++ StockTicker/StockModel/README.txt
@@ -0,0 +1,1 @@
+﻿This is where the Model will live.
